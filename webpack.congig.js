34a6dc610ce213e589abc78c9fb3f4528bde091a
Eyvgeniy <antonm33@yandex.ru>
2019-10-13 21:12:22 +0200

    second commit

---
+++ webpack.congig.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+
+module.exports = {
+  entry: 'index.js',
+  output: {
+    path: path.resolve(__dirname, '/dist'),
+    filename: 'index_bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: '/node_modules/' }
+    ]
+  }
+}
