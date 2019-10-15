ff9679576ff3ae1f14047f97ea4076b3169cb568
Eyvgeniy <antonm33@yandex.ru>
2019-10-15 22:40:33 +0200

    add validation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import validator from 'validator';
+import { watch } from 'melanke-watchjs';
 
-const func = (a, b) => a + b;
-console.log(func(3, 2));
+const state = {
+  urlValid: false
+};
+
+const rssUrl = document.getElementById('rssUrl');
+rssUrl.addEventListener('keyup', () => {
+  const url = rssUrl.value;
+  state.urlValid = validator.isURL(url);
+});
+
+watch(state, 'urlValid', () => {
+  const button = document.getElementById('button');
+  if (state.urlValid === true) {
+    button.removeAttribute('disabled');
+    button.classList.remove('btn-danger');
+    button.classList.add('btn-primary');
+    console.log('Good');
+  } else {
+    button.setAttribute('disabled', 'disabled');
+    button.classList.remove('btn-primary');
+    button.classList.add('btn-danger');
+    console.log('Bad');
+  }
+});
